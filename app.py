5c8952c980a030d541dcbf89e75ff98e1dcf50dc
thanushSheri <thanushsheri2@gmai.com>
2026-04-26 13:56:32 +0530

    ready for deployment

--- app.py
+++ app.py
@@ -93,3 +93,9 @@ def product_detail(item_id):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+    import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
